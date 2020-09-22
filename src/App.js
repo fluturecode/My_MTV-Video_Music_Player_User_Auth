338a209331b021a4c840a83d1c314a68c45a924b
Elizabeth Eidelson <fluturecode@gmail.com>
2020-09-22 19:24:02 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 import Video from "./components/Video";
 
 function App() {
 	return (
 		<div className="app">
-			{/* <Header /> */}
+			<Header />
 			<Video />
-			{/* <SignUp /> */}
 		</div>
 	);
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+	return (
+		<div className="header">
+			<img
+				className="header__logo"
+				src="http://cdn30.us1.fansshare.com/image/mtv/ba-mtv-logo-gallery-mtv-logo-clipart-mtv-198356865.jpg"
+				alt="MTV logo"
+			/>
+		</div>
+	);
+}
+
+export default Header;
