03c04143f25bcf69284dec90766163fc438389dc
Elizabeth Eidelson <fluturecode@gmail.com>
2020-09-24 21:30:06 -0400

    updated layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./Header.css";
+import logo from "../images/download.png";
 
 function Header() {
 	return (
 		<div className="header">
-			<img
-				className="header__logo"
-				src="https://i.pinimg.com/originals/31/9b/62/319b62c09cd6148520ff471625105056.jpg"
-				alt="MTV logo"
-			/>
+			<img className="header__logo" src={logo} alt="MTV logo" />
 		</div>
 	);
 }
