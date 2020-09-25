323d877b50b7a749fb273af1926fb94f733f5305
Elizabeth Eidelson <fluturecode@gmail.com>
2020-09-24 20:47:53 -0400

    added material-UI buttons and styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Video from "./components/Video";
 import { db } from "./firebase";
+import { Button } from "@material-ui/core";
 
 function App() {
 	const [artist, setArtist] = useState("");
@@ -29,16 +30,6 @@ function App() {
 			artist: artist,
 			videoId: videoId,
 		});
-
-		// setVideos([
-		// {
-		// 	title: title,
-		// 	artist: artist,
-		// 	videoId: tvideoId,
-		// },
-		// 	...videos,
-		// ]);
-
 		setArtist("");
 		setTitle("");
 		setVideoId("");
@@ -47,38 +38,38 @@ function App() {
 	return (
 		<div className="app">
 			<Header />
-
-			<form>
-				<input
-					placeholder="artist..."
-					value={artist}
-					onChange={(e) => setArtist(e.target.value)}
-				/>
-				<input
-					placeholder="title..."
-					value={title}
-					onChange={(e) => setTitle(e.target.value)}
-				/>
-				<input
-					placeholder="videoId..."
-					value={videoId}
-					onChange={(e) => setVideoId(e.target.value)}
-				/>
-				<button type="submit" onClick={handleClick}>
-					Add video
-				</button>
-			</form>
-
-			{videos.map(({ id, data }) => (
-				<Video
-					key={id}
-					title={data.title}
-					artist={data.artist}
-					video={data.videoId}
-				/>
-			))}
-
-			{/* <Video title="Billy Jean" artist="Michael Jackson" video="VVj1m8uy5AM" /> */}
+			<div className="app__form">
+				<form>
+					<input
+						placeholder="artist..."
+						value={artist}
+						onChange={(e) => setArtist(e.target.value)}
+					/>
+					<input
+						placeholder="title..."
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+					<input
+						placeholder="videoId..."
+						value={videoId}
+						onChange={(e) => setVideoId(e.target.value)}
+					/>
+					<Button variant="contained" type="submit" onClick={handleClick}>
+						Add video
+					</Button>
+				</form>
+			</div>
+			<div className="app__video">
+				{videos.map(({ id, data }) => (
+					<Video
+						key={id}
+						title={data.title}
+						artist={data.artist}
+						video={data.videoId}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
