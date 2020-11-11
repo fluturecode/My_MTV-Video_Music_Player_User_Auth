d8e63d00cd986b3619858d09b55fb8e3baea4f49
Liz Eidelson <fluturecode@gmail.com>
2020-11-10 19:33:11 -0500

    removed firebase login

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Home from "./components/Home.js";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/Login.js";
-import SignUp from "./components/SignUp.js";
-import { auth } from "./firebase";
-import { useStateValue } from "./context/StateProvider.js";
+import React, { useEffect } from "react"
+import "./App.css"
+import Header from "./components/Header"
+import Home from "./components/Home.js"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./components/Login.js"
+import SignUp from "./components/SignUp.js"
+import { auth } from "./firebase"
+import { useStateValue } from "./context/StateProvider.js"
 
 function App() {
-	const [{}, dispatch] = useStateValue;
+	// const [{}, dispatch] = useStateValue
 
-	useEffect(() => {
-		// will only run once when the app component loads...
-		auth.onAuthStateChanged((authUser) => {
-			if (authUser) {
-				// the user just logged in / the user was logged in
-				dispatch({
-					type: "SET_USER",
-					user: authUser,
-				});
-			} else {
-				// the user is logged out
-				dispatch({
-					type: "SET_USER",
-					user: null,
-				});
-			}
-		});
-	}, [dispatch]);
+	// useEffect(() => {
+	// 	// will only run once when the app component loads...
+	// 	auth.onAuthStateChanged((authUser) => {
+	// 		if (authUser) {
+	// 			// the user just logged in / the user was logged in
+	// 			dispatch({
+	// 				type: "SET_USER",
+	// 				user: authUser,
+	// 			})
+	// 		} else {
+	// 			// the user is logged out
+	// 			dispatch({
+	// 				type: "SET_USER",
+	// 				user: null,
+	// 			})
+	// 		}
+	// 	})
+	// }, [dispatch])
 
 	return (
 		<Router>
@@ -47,7 +47,7 @@ function App() {
 				</Switch>
 			</div>
 		</Router>
-	);
+	)
 }
 
-export default App;
+export default App
